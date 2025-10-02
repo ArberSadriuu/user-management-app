1da6d155da4368abad9ffacb925e89facfba9a65
Arber <149020732+ArberSadriuu@users.noreply.github.com>
2025-10-02 21:31:15 +0200

    feat: created api for fetching users and users by id and its types

---
+++ src/api/users.ts
@@ -0,0 +1,19 @@
+import type { User } from "../../types"
+
+const API_BASE_URL = "https://jsonplaceholder.typicode.com"
+
+export const fetchUsers = async (): Promise<User[]> => {
+    const response = await fetch(`${API_BASE_URL}/users`)
+    if (!response.ok) {
+        throw new Error("Failed to fetch users")
+    }
+    return response.json()
+}
+
+export const fetchUserById = async (id: string): Promise<User> => {
+    const response = await fetch(`${API_BASE_URL}/users/${id}`)
+    if (!response.ok) {
+        throw new Error("Failed to fetch users")
+    }
+    return response.json()
+}
